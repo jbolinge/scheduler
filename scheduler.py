6c3da25365a5b0cf027a15d451e59abf11c64380
me <jbolinge@hotmail.com>
2015-04-12 22:00:31 -0500

    made importable, tweaked cost

--- scheduler.py
+++ scheduler.py
@@ -18,11 +18,11 @@ def cost(schedule_cand):
 	for i in range(1, len(schedule_cand)):
 		if (np.all(schedule_cand[i])):
 			if (schedule_cand[i][3] == schedule_cand[i-1][3]):   #Cost to be at MVSC consecutive weeks
-				tot_cost += 10
+				tot_cost += 8
 			if (schedule_cand[i][1] == schedule_cand[i-1][1]):   #Cost to be long consecutive weeks
 				tot_cost += 5
 			if (i > 1 and schedule_cand[i][0] == schedule_cand[i-2][0]):	#Cost to be on call 2 times in 3 weeks
-				tot_cost += 3
+				tot_cost += 6
 			if (schedule_cand[i][4] == schedule_cand[i-1][0] or schedule_cand[i][4] == schedule_cand[i-1][2]):	 #Cost reduction to be on call or early prior to vacation
 				tot_cost -= 1
 			tot_cost += 1
@@ -32,14 +32,10 @@ def is_schedule_valid(schedule_cand, target_num):			#Check if valid schedule (ap
 	div_factor = target_num / 53.
 	for i in range(1,6):
 		if (schedule_cand[1:target_num,0]==i).sum() > math.ceil(11 * div_factor):	#Equal number of call weeks
-			#print 'invalid for call'
 			return False
 		if (schedule_cand[1:target_num,2]==i).sum() > math.ceil(12 * div_factor):	#Equal number of early weeks
-			#print 'invalid for early'
-			#print schedule_cand
 			return False
 		if (schedule_cand[1:target_num,3]==i).sum() > math.ceil(18 * div_factor):	#Equal number of MVSC weeks
-			#print 'invalid for MVSC'
 			return False
 	return True
 
@@ -71,19 +67,15 @@ def no_prune(schedule_cand, week_num):
 	if (best_cost < 9999):
 		cs = cost(schedule_cand)
 		if (cs > best_cost or cs > overall_best_cost):
-			#print 'pruned for cost %d' % week_num
 			return False
 	if (week_num > 7):
 		for i in range(1,6):
 			if (schedule_cand[week_num-8:week_num,0]==i).sum() < 1:	#at least 1 call every 8 weeks
-				#print 'pruned for call %d' % week_num
 				return False
 			if (schedule_cand[week_num-8:week_num,2]==i).sum() < 1:	#at least 1 early every 8 weeks
-				#print 'pruned for early %d' % week_num
 				return False
 			if i < 3:
 				if (schedule_cand[week_num-6:week_num,3]==i).sum() < 1:	#at least 1 mvsc every 6 weeks
-					#print 'pruned for mvsc %d' % week_num
 					return False
 	return True
 
@@ -97,17 +89,14 @@ def gen_schedules(week_num, schedule_cand, target_num):		#Generate schedule cand
 			cs = cost(schedule_cand)
 			if (cs < best_cost and cs < overall_best_cost):
 				if (target_num == 53):
-					#if (cs < overall_best_cost):
 					schedules.append(np.array(schedule_cand))
 					overall_best_cost = cs
 					best_cost = cs
-					print 'best_cost %d' % best_cost
 					logging.info('candidate found, cost %d', overall_best_cost)
 				else:
 					temp = cs
 					best_cost = target_num + 13
 					logging.info('week %d, cost %d', week_num, cs)
-					print 'week %d best_cost %d' % (week_num, cs)
 					gen_schedules(target_num, schedule_cand, target_num + 13)
 					best_cost = temp
 		return
@@ -127,7 +116,7 @@ def read_input(filename):
 		lines = [[int(s) for s in i.split(',')] for i in file_data]
 		assert len(lines) == 13
 	except:
-		logging.info('error opening schedule file %s',filename)
+		logging.error('error opening schedule file %s',filename)
 		print 'file format invalid: all numbers separated by commas'
 		print 'line 1 is last week of previous year'
 		print 'line 2 is first week call from previous year'
@@ -165,30 +154,29 @@ def set_calls(schedule_cand, emp_num, list_weeks, is_call=True):
 
 def save_result(filename):
 	np.savetxt(filename, schedules[-1], delimiter=',')
+	logging.info('saved schedule to %s', filename)
 
-if __name__ == "__main__":
-	logging.basicConfig(filename='scheduler.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
-	sys.setrecursionlimit(1000)
-	try:
-		filename = sys.argv[1]
-		savename = sys.argv[2]
-	except:
-		logging.info('error with command line arguments')
-		print'Please supply the name of the input file and output file'
-		sys.exit()
+def run_schedule(s_filename, savename):
 	global schedules, perm_list, best_cost, overall_best_cost
+	logging.basicConfig(filename=savename + '.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO, mode='w')
+	sys.setrecursionlimit(1000)
 	overall_best_cost = 9999
 	schedules = list()
 	perm_list = gen_permutations(5, [1, 2, 3])
 	best_cost = 14
-	person_dict = {1:'Jason', 2:'Brian', 3:'Dick', 4:'Mark', 5:'Tim'}
-	position_dict = {0:'Call', 1:'Long', 2:'Short', 3:'MVSC', 4:'Vacation'}
-	schedule_cand = read_input(filename)
-	print schedule_cand
+	schedule_cand = read_input(s_filename)
 	logging.info('beginning schedule generation')
-	print 'Generating schedules'
 	gen_schedules(1, schedule_cand, 14)
 	logging.info('finished schedule generation, best cost %d', cost(schedules[-1]))
-	print schedules
-	print 'final best cost: %d' % cost(schedules[-1])
 	save_result(savename)
+
+if __name__ == "__main__":
+	try:
+		s_filename = sys.argv[1]
+		savename = sys.argv[2]
+	except:
+		print'Please supply the name of the input file and output file'
+		sys.exit()
+	person_dict = {1:'Jason', 2:'Brian', 3:'Dick', 4:'Mark', 5:'Tim'}
+	position_dict = {0:'Call', 1:'Long', 2:'Short', 3:'MVSC', 4:'Vacation'}
+	run_schedule(s_filename, savename)	
